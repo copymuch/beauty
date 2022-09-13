a09740b98f74f056268662129261af2c504648c8
Mahnev <Mahnev.web@gmail.com>
2022-09-14 00:58:56 +0700

    project_initilization

---
+++ js/anim.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('header').addClass('show');
+	$(window).scroll(function(){
+		if ($(this).scrollTop() ==  0){
+		} else {
+			if ($(this).scrollTop() > 100){
+				$('.about').addClass('scroll');
+		}
+		}
+	});
+});
